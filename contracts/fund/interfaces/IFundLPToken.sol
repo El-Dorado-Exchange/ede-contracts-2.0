0181663d0846c389b6b14a56c4170b470240f337
frank <116536040+ede-frank@users.noreply.github.com>
2023-03-01 18:01:07 +0800

    Delete contracts/fund directory

--- contracts/fund/interfaces/IFundLPToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-
-interface IFundLPToken {
-    function mint(address _account, uint256 _amount) external;
-    function burn(uint256 _amount) external;
-}
